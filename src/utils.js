803a79a99645145701b7e2a264097901067f511e
Andrew Li <andrewli357@gmail.com>
2019-12-21 17:16:09 -0600

    LaTeX + Medium

---
+++ src/utils.js
@@ -0,0 +1,33 @@
+const LATEX_USEPACKAGE_STATEMENT = /(\\usepackage.+)/g;
+
+const QUICKLATEX_API = 'https://quicklatex.com/latex3.f';
+
+async function createMathImage(rawMath, fontSize, fontColor) {
+  const preambleStatements = rawMath.match(LATEX_USEPACKAGE_STATEMENT);
+  const preamble = preambleStatements !== null ? preambleStatements.join('\n') : '';
+  const math = rawMath.replace(LATEX_USEPACKAGE_STATEMENT, '');
+
+  const response = await fetch(QUICKLATEX_API, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-urlencoded' },
+    body: `preamble=${preamble}&formula=${encodeURIComponent(math)}&fsize=${`${fontSize}px`}&fcolor=${fontColor}`,
+  });
+  const text = await response.text();
+  const secondLine = text.split('\n')[1];
+  const [url, , width, height] = secondLine.split(' ');
+  return [url, width, height];
+}
+
+async function createMathImageFile(imageUri) {
+  const response = await fetch(imageUri);
+  const blob = await response.blob();
+  const fileName = imageUri.slice(imageUri.lastIndexOf('/') + 1);
+  return new File([blob], fileName, { type: 'image/png' });
+}
+
+function placeFileIntoPost(file) {
+  const dataTransfer = new DataTransfer();
+  dataTransfer.items.add(file);
+  const event = new DragEvent('drop', { dataTransfer });
+  document.body.dispatchEvent(event);
+}
