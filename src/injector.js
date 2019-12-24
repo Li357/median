86986f902b4815fad4b5fbaef4ebc928e4e3c6f1
Andrew Li <andrewli357@gmail.com>
2019-12-24 00:26:20 -0600

    Fix post corruption when deleting raw math

--- src/injector.js
+++ src/injector.js
@@ -6,6 +6,8 @@ const HIGHLIGHTMENU_TEXT_BUTTONS_CONTAINER = `${HIGHLIGHTMENU_TEXT} > .highlight
 const HIGHLIGHTMENU_TEXT_DIVIDER = `${HIGHLIGHTMENU_TEXT_BUTTONS_CONTAINER} > .buttonSet-separator`;
 const HIGHLIGHTMENU_BUTTON = `${HIGHLIGHTMENU_TEXT_BUTTONS_CONTAINER} > .button--highlightMenu[data-action=link]`; // Link icon has no state
 
+const POST_CONTENT = '.postArticle-content';
+
 const SIGMA_PATH = 'M3.63,2.05v.56l6.53,8.08L3.63,18.52V19l12.75-.08c.16-1.62.48-3.23.73-4.76l-.41-.08a5.73,5.73,0,0,1-1.45,2.42,5.31,5.31,0,0,1-3.15.56H6.21l6-7.26L6.77,3.1H9.92c2,0,4.85-.49,5.65,1.94a3.33,3.33,0,0,1,.24,1.29h.49L16,2.13H3.63Z';
 
 function createHighlightMenuButton(type, size, iconPath) {
@@ -19,17 +21,18 @@ function createHighlightMenuButton(type, size, iconPath) {
 async function injectMath(rawMath, fontSize, fontColor, x, y) {
   const uri = await createMathImage(rawMath, fontSize, fontColor);
   const file = await createMathImageFile(uri);
+  const event = new KeyboardEvent('keydown', { key: 'Backspace' }); // can't remove range directly (or else corrupts Medium post)
+  document.querySelector(POST_CONTENT).dispatchEvent(event);
   placeFileIntoPost(file, x, y);
 }
 
 function injectMedian() {
   const median = createHighlightMenuButton('math', 21, SIGMA_PATH);
-  median.addEventListener('click', async () => {
+  median.addEventListener('click', () => {
     const selection = window.getSelection();
     const selectionText = selection.toString();
     const { x, y } = selection.getRangeAt(0).getBoundingClientRect();
-    await injectMath(selectionText, 30, '000000', x, y);
-    selection.deleteFromDocument();
+    injectMath(selectionText, 30, '000000', x, y);
   });
 
   const divider = document.querySelector(HIGHLIGHTMENU_TEXT_DIVIDER).cloneNode(true);
